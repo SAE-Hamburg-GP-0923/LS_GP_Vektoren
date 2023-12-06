dea3aa82544bcbd32a830f65f56fc788b885ec23
Leon Schultz <Prodbytxnshi@gmail.com>
2023-12-07 00:06:48 +0100

    VC Setup

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace LS_GP_Vektoren;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // x: 0 y: 0; z: 0;
+        Vector defaultVector = new Vector();
+        Console.WriteLine($"Default Constructor Vector: {defaultVector.ToString()}\n");
+
+        // x: 25 y: 25; z: 25;
+        Vector customVector = new Vector(25f, 25f, 25f);
+        Console.WriteLine($"Custom Constructor Vector: {customVector.ToString()}\n");
+
+        // x: 300; y: 300; z: 300;
+        Vector addVec1 = new Vector(100f, 100f, 100f);
+        Vector addVec2 = new Vector(200f, 200f, 200f);
+        Console.WriteLine($"Addition: {addVec1 + addVec2}\n");
+        
+        // x: 50; y: 75; z: 75;
+        Vector subVec1 = new Vector(100f, 100f, 100f);
+        Vector subVec2= new Vector(50f, 25f, 25f);
+        Console.WriteLine($"Subtraction: {subVec1 - subVec2}\n");
+        
+        // x: 1000; y: 1000; z: 1000;
+        Vector mulVec = new Vector(100f, 100f, 100f);
+        float scalar = 10;
+        Console.WriteLine($"Multiplication: {subVec1 * scalar}\n");
+        
+        // Distance
+        Vector distVec1 = new Vector(20f, 20f, 20f);
+        Vector distVec2 = new Vector(50f, 50f, 50f);
+        Console.WriteLine($"Distance: {distVec1.GetDistance(distVec2)}\n");
+        
+        // Magnitude
+        Vector magVec = new Vector(25f, 25f, 25f);
+        Console.WriteLine($"Magnitude: {magVec.GetMagnitude()}");
+        
+        // Square Magnitude
+        Vector sqrMagVec = new Vector(100f, 100f, 100f);
+        Console.WriteLine($"Square Magnitude: {sqrMagVec.GetSquareMagnitude()}");
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,66 @@
+﻿namespace LS_GP_Vektoren;
+
+public class Vector
+{
+    // Drei Float Attribute
+    public float m_x = 0;
+    public float m_y = 0;
+    public float m_z = 0;
+
+    // Standard Vektor in dem x,y,z auf 0 gesetzt werden
+    public Vector()
+    {
+        m_x = 0;
+        m_y = 0;
+        m_z = 0;
+    }
+
+    // Einen Konstruktor in dem x,y,z mit Parametern initialisiert werden
+    public Vector(float x, float y, float z)
+    {
+        m_x = x;
+        m_y = y;
+        m_z = z;
+    }
+
+    // Operator overloading
+    public static Vector operator +(Vector lhs, Vector rhs) =>
+        new Vector(lhs.m_x + rhs.m_x, lhs.m_y + rhs.m_y, lhs.m_z + rhs.m_z);
+
+    public static Vector operator -(Vector lhs, Vector rhs) =>
+        new Vector(lhs.m_x - rhs.m_x, lhs.m_y - rhs.m_y, lhs.m_z - rhs.m_z);
+
+    public static Vector operator *(Vector vec, float scalar) =>
+        new Vector(vec.m_x * scalar, vec.m_y * scalar, vec.m_z * scalar);
+
+    // Non-Static Methode um die Distanz zwischen dem und einem anderen Vector
+    public float GetDistance(Vector other)
+    {
+        return MathF.Sqrt(MathF.Pow(m_x - other.m_x, 2) + MathF.Pow(m_y - other.m_y, 2) +
+                          MathF.Pow(m_z - other.m_z, 2));
+    }
+
+    // Non-Static Methode um die Distanz zwischen zwei Vektoren zu berechnen
+    public static float GetDistance(Vector lhs, Vector rhs)
+    {
+        return MathF.Sqrt(MathF.Pow(lhs.m_x - rhs.m_x, 2) + MathF.Pow(lhs.m_y - rhs.m_y, 2) +
+                          MathF.Pow(lhs.m_z - rhs.m_z, 2));
+    }
+
+    public override string ToString()
+    {
+        return $"{m_x} : {m_y} : {m_z}";
+    }
+    
+    // Berechnet die länge des Vektors
+    public float GetMagnitude()
+    {
+        return MathF.Sqrt(MathF.Pow(m_x, 2f) + MathF.Pow(m_y, 2f) + MathF.Pow(m_z, 2f));
+    }
+    
+    // Berechnet die länge des Vektors
+    public float GetSquareMagnitude()
+    {
+        return MathF.Pow(m_x, 2f) + MathF.Pow(m_y, 2f) + MathF.Pow(m_z, 2f);
+    }
+}
