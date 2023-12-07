854f935722477e1ebf2900fa4831eedc597f9205
Leon Schultz <Prodbytxnshi@gmail.com>
2023-12-07 15:03:04 +0100

    Refactored Vector Functions

--- Program.cs
+++ Program.cs
@@ -7,34 +7,34 @@ class Program
     {
         // x: 0 y: 0; z: 0;
         Vector defaultVector = new Vector();
-        Console.WriteLine($"Default Constructor Vector: {defaultVector.ToString()}\n");
+        Console.WriteLine($"Default Constructor Vector: {defaultVector.ToString()}");
 
         // x: 25 y: 25; z: 25;
         Vector customVector = new Vector(25f, 25f, 25f);
-        Console.WriteLine($"Custom Constructor Vector: {customVector.ToString()}\n");
+        Console.WriteLine($"Custom Constructor Vector: {customVector.ToString()}");
 
         // x: 300; y: 300; z: 300;
         Vector addVec1 = new Vector(100f, 100f, 100f);
         Vector addVec2 = new Vector(200f, 200f, 200f);
-        Console.WriteLine($"Addition: {addVec1 + addVec2}\n");
+        Console.WriteLine($"Addition: {addVec1 + addVec2}");
         
         // x: 50; y: 75; z: 75;
         Vector subVec1 = new Vector(100f, 100f, 100f);
         Vector subVec2= new Vector(50f, 25f, 25f);
-        Console.WriteLine($"Subtraction: {subVec1 - subVec2}\n");
+        Console.WriteLine($"Subtraction: {subVec1 - subVec2}");
         
         // x: 1000; y: 1000; z: 1000;
         Vector mulVec = new Vector(100f, 100f, 100f);
-        float scalar = 10;
-        Console.WriteLine($"Multiplication: {subVec1 * scalar}\n");
+        const float scalar = 10f;
+        Console.WriteLine($"Multiplication: {subVec1 * scalar}");
         
         // Distance
         Vector distVec1 = new Vector(20f, 20f, 20f);
         Vector distVec2 = new Vector(50f, 50f, 50f);
-        Console.WriteLine($"Distance: {distVec1.GetDistance(distVec2)}\n");
+        Console.WriteLine($"Distance: {distVec1.GetDistance(distVec2)}");
         
         // Magnitude
-        Vector magVec = new Vector(25f, 25f, 25f);
+        Vector magVec = new Vector(1f, 2f, 3f);
         Console.WriteLine($"Magnitude: {magVec.GetMagnitude()}");
         
         // Square Magnitude
--- Vector.cs
+++ Vector.cs
@@ -33,34 +33,33 @@ public class Vector
     public static Vector operator *(Vector vec, float scalar) =>
         new Vector(vec.m_x * scalar, vec.m_y * scalar, vec.m_z * scalar);
 
-    // Non-Static Methode um die Distanz zwischen dem und einem anderen Vector
-    public float GetDistance(Vector other)
-    {
-        return MathF.Sqrt(MathF.Pow(m_x - other.m_x, 2) + MathF.Pow(m_y - other.m_y, 2) +
-                          MathF.Pow(m_z - other.m_z, 2));
-    }
-
     // Non-Static Methode um die Distanz zwischen zwei Vektoren zu berechnen
     public static float GetDistance(Vector lhs, Vector rhs)
     {
         return MathF.Sqrt(MathF.Pow(lhs.m_x - rhs.m_x, 2) + MathF.Pow(lhs.m_y - rhs.m_y, 2) +
                           MathF.Pow(lhs.m_z - rhs.m_z, 2));
     }
+    
+    // Non-Static Methode um die Distanz zwischen dem und einem anderen Vector
+    public float GetDistance(Vector other)
+    {
+        return GetDistance(this, other);
+    }
 
     public override string ToString()
     {
         return $"{m_x} : {m_y} : {m_z}";
     }
-    
-    // Berechnet die länge des Vektors
-    public float GetMagnitude()
-    {
-        return MathF.Sqrt(MathF.Pow(m_x, 2f) + MathF.Pow(m_y, 2f) + MathF.Pow(m_z, 2f));
-    }
-    
-    // Berechnet die länge des Vektors
+
+    // Berechnet die Quadratlänge des Vektors
     public float GetSquareMagnitude()
     {
         return MathF.Pow(m_x, 2f) + MathF.Pow(m_y, 2f) + MathF.Pow(m_z, 2f);
+    }
+
+    // Berechnet die länge des Vektors
+    public float GetMagnitude()
+    {
+        return MathF.Sqrt(GetSquareMagnitude());
     }
 }
